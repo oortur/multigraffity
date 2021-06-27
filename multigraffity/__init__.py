886996111934feda919418e43dab308d51a6c793
imArtyfact <rtgenius0702@gmail.com>
2021-06-27 16:56:28 +0300

    add GEMSEC algorithm

---
+++ multigraffity/__init__.py
@@ -0,0 +1,5 @@
+from .walkers import MultiRandomWalker, MultiRandomWalkerWithHops
+from .gemsec import GEMSEC, MultiGEMSEC
+
+
+__all__ = ["GEMSEC", "MultiGEMSEC", "MultiRandomWalker", "MultiRandomWalkerWithHops"]
